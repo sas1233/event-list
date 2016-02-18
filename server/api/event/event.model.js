a6c83e54ed17a07c6a1d4f8e0e927534eeb4175d
gernets <gernets@segater.com>
2016-02-18 21:17:00 +0200

    event endpoint

---
+++ server/api/event/event.model.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var EventSchema = new Schema({
+    _user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  address: String,
+  name: String,
+  description: String,
+  startDate: Date,
+  endDate: Date,
+  isActive: Boolean,
+  created: {
+      type: Date,
+      default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('Event', EventSchema);
